1002f482d9ef990da66db02aa508e867e0c3330d
akop4 <akopov.andrew@gmail.com>
2023-05-16 12:20:17 +0300

    Loaded examples from the_self_taught_programmer

---
+++ the_self_taught_programmer/python_ex292.py
@@ -0,0 +1,18 @@
+# Thanks so much for reading my book. Feel free to contact me at cory[at]theselftaughtprogrammer.io.
+
+### GOOGLE CHANGED THEIR CODE. Try using 'articles' instead of 'html'
+
+
+def scrape(self):
+    r = urllib.request\
+        .urlopen(self.site)
+    html = r.read()
+    parser = "html.parser"
+    sp = BeautifulSoup(html,
+                       parser)
+    for tag in sp.find_all("a"):
+        url = tag.get("href")
+        if url is None:
+            continue
+        if "html" in url:
+            print("\n" + url)
