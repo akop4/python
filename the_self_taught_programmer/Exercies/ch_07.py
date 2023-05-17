0f33159497318be6e3bd59dd0b0b5c60e495dd00
akop4 <akopov.andrew@gmail.com>
2023-05-17 15:14:53 +0300

    ch7

--- the_self_taught_programmer/Exercies/ch_07.py
+++ the_self_taught_programmer/Exercies/ch_07.py
@@ -38,30 +38,32 @@ for i in lst_anime_tv:
     break
 
 # Challenge 4
-max_range = 2**1000
-rng = random.randrange(1,max_range)
+max_range = 10 ** 10
+rng = random.randrange(1, max_range)
 print('Right number:', rng)
 low = 1
 high = max_range - 1
 try_count = 0
 while True:
-    inp = int(low + high) // 2
+    mid = int(low + high) // 2
+    # inp = int(input())
+    inp = mid
     # print('input:', inp)
     if inp == 'q':
-        # print('Exit')
+        print('Exit')
         break
     try:
         if int(inp) == rng:
-            print('You are win in',try_count,'steps')
+            print(inp,' is right number\n','You are win in ',try_count,' steps',sep='')
             break
         elif int(inp) > max_range or int(inp) < 1:
-            # print('Out of range')
+            print(inp,'is out of range')
             break
         elif int(inp) > rng:
-            # print('Too big')
+            print(inp,'is too big')
             high = inp - 1
         else:
-            # print('Too small')
+            print(inp,'is too small')
             low = inp + 1
     except (ValueError):
         print('Bad value of input')
