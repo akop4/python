aa7049942be0ee22c61093664df7368a598da684
akop4 <akopov.andrew@gmail.com>
2023-05-19 19:56:20 +0300

    txt

--- the_self_taught_programmer/Exercies/ch_09.py
+++ the_self_taught_programmer/Exercies/ch_09.py
@@ -1,8 +1,17 @@
-# import os
+import os
 '''cwd = os.getcwd()  # Get the current working directory (cwd)
 files = os.listdir(cwd)  # Get all the files in that directory
 print("Files in %r: %s" % (cwd, files))
 '''
 # Challenge 1
-with open("ch_09.txt", "r") as txt_csv:
-    print(txt_csv.read())
+path_to_file = os.path.join('the_self_taught_programmer', 'Exercies', 'ch_09_1.txt')
+with open(path_to_file, "r") as txt_file:
+    print(txt_file.read())
+
+# print('Challenge 1')
+
+# Challenge 2
+path_to_file = os.path.join('the_self_taught_programmer', 'Exercies', 'ch_09_2.txt')
+with open("ch_09_answer.txt", "w") as txt_file:
+    txt_file.write(input('Write text:\n'))
+    # print('Challenge 2')
