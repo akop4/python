4b8b2959c543f0e34193851b00f17c8807b8b87e
akop4 <akopov.andrew@gmail.com>
2023-05-19 15:41:46 +0300

    '09'

--- the_self_taught_programmer/Exercies/ch_09.py
+++ the_self_taught_programmer/Exercies/ch_09.py
@@ -1,4 +1,8 @@
-import os
+# import os
+'''cwd = os.getcwd()  # Get the current working directory (cwd)
+files = os.listdir(cwd)  # Get all the files in that directory
+print("Files in %r: %s" % (cwd, files))
+'''
 # Challenge 1
-with open("ch_09.txt ", "r") as txt_csv:
+with open("ch_09.txt", "r") as txt_csv:
     print(txt_csv.read())
