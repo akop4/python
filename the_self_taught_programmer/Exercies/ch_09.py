bdd85af73d6161f85f527874fcb21d8704c7efb4
akop4 <akopov.andrew@gmail.com>
2023-05-19 20:11:05 +0300

    ch_9

--- the_self_taught_programmer/Exercies/ch_09.py
+++ the_self_taught_programmer/Exercies/ch_09.py
@@ -1,17 +1,23 @@
-import os
-'''cwd = os.getcwd()  # Get the current working directory (cwd)
-files = os.listdir(cwd)  # Get all the files in that directory
-print("Files in %r: %s" % (cwd, files))
-'''
-# Challenge 1
-path_to_file = os.path.join('the_self_taught_programmer', 'Exercies', 'ch_09_1.txt')
-with open(path_to_file, "r") as txt_file:
-    print(txt_file.read())
+import os, csv
 
-# print('Challenge 1')
+# Challenge 1
+def reading_txt_file(path_to_file):
+    with open(path_to_file, "r") as txt_file:
+        print(txt_file.read())
+path_to_file = os.path.join('the_self_taught_programmer', 'Exercies', 'ch_09_1.txt')
+reading_txt_file(path_to_file)
 
 # Challenge 2
 path_to_file = os.path.join('the_self_taught_programmer', 'Exercies', 'ch_09_2.txt')
-with open("ch_09_answer.txt", "w") as txt_file:
-    txt_file.write(input('Write text:\n'))
-    # print('Challenge 2')
+with open(path_to_file, "w") as txt_file:
+    txt_file.write('Challenge 2')
+reading_txt_file(path_to_file)
+
+# Challenge 3
+list = [['one', 'two', 'three'], ['four', 'five', 'six'], ['seven', 'eight', 'nine']]
+path_to_file = os.path.join('the_self_taught_programmer', 'Exercies', 'ch_09_3.txt')
+with open(path_to_file, "w") as txt_file:
+    csv_file = csv.writer(txt_file,delimiter=',')
+    # for i in list:
+    csv_file.writerows(list)
+reading_txt_file(path_to_file)
